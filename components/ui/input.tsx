5e24da2542f9b818b12daefb0ee2c7b7ffc20fee
Muhammad Faiq Mubarok <126318140+faiqmubarok@users.noreply.github.com>
2025-07-16 02:56:22 +0700

    Feature/FE 004 class student (#10)
    * feat(class): implement student class page with join form and conditional UI
    
    - Create class page for student
    - Fetch classes by userId via API
    - Add join class form with validation
    - Display different UI for empty and non-empty class states
    
    * feat(class-student): implement detail class page for student role
    
    - Built the Detail Class page including assignment and classmate views.
    - Modified Avatar, Dialog, and Input components to fit project requirements.
    - Added new UI components: Calendar, Popover, Separator, Tabs, ToggleGroup, and Toggle (from shadcn).
    - Created custom hooks: use-debounce and use-search-params for managing debounced inputs and URL parameters.

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,20 +1,59 @@
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
+import { LucideIcon } from "lucide-react";
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  startIcon?: LucideIcon;
+  endIcon?: LucideIcon;
+  onClickStartIcon?: () => void;
+  onClickEndIcon?: () => void;
+}
+
+function Input({
+  className,
+  type,
+  endIcon,
+  startIcon,
+  onClickStartIcon,
+  onClickEndIcon,
+  ...props
+}: InputProps) {
+  const StartIcon = startIcon;
+  const EndIcon = endIcon;
+
   return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-12 w-full min-w-0 rounded-full border bg-transparent px-5 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className
+    <div className="relative w-full">
+      {StartIcon && (
+        <div className="absolute top-1/2 left-4.5 -translate-y-1/2">
+          <StartIcon
+            size={18}
+            className="text-muted-foreground"
+            onClick={onClickStartIcon}
+          />
+        </div>
       )}
-      {...props}
-    />
+      <input
+        type={type}
+        data-slot="input"
+        className={cn(
+          "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-12 w-full min-w-0 rounded-full border bg-transparent px-5 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+          "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+          className,
+        )}
+        {...props}
+      />
+      {EndIcon && (
+        <div className="absolute top-1/2 right-4.5 -translate-y-1/2">
+          <EndIcon
+            size={18}
+            className="text-foreground"
+            onClick={onClickEndIcon}
+          />
+        </div>
+      )}
+    </div>
   );
 }
 
