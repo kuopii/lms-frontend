f167748db864ae103876b7d0754a27f6b93e6267
Wisnu Sadewa <103896171+wisnusadewa@users.noreply.github.com>
2025-09-09 10:27:08 +0700

    refactor: simplify AudioDropzone and ImageDropzone components  (#46)
    * refactor: simplify AudioDropzone and ImageDropzone components by removing Controller wrapper
    
    * refactor: remove unused useFormContext import from AudioDropzone and ImageDropzone components
    
    ---------
    
    Co-authored-by: Muhammad Faiq Mubarok <faiqmubarok@gmail.com>

--- features/test/listening/components/image-dropzone.tsx
+++ features/test/listening/components/image-dropzone.tsx
@@ -1,7 +1,7 @@
 import LazyImage from "@/components/imageReusable/base/LazyImage";
 import { useState } from "react";
 import { useDropzone } from "react-dropzone";
-import { Controller, useFormContext } from "react-hook-form";
+import { useController } from "react-hook-form";
 import { FaUpload } from "react-icons/fa6";
 
 interface ImageDropzoneProps {
@@ -9,82 +9,69 @@ interface ImageDropzoneProps {
 }
 
 export const ImageDropzone = ({ fieldPrefix }: ImageDropzoneProps) => {
-  const {
-    control,
-    // formState: { errors },
-  } = useFormContext();
+  const { field } = useController({ name: fieldPrefix });
 
   const [previewUrl, setPreviewUrl] = useState<string>();
   console.log("previewUrl ??", previewUrl);
 
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    multiple: false,
+    accept: {
+      "image/jpeg": [".jpeg", ".jpg"],
+      "image/png": [".png"],
+    },
+    onDrop: (acceptedFiles: File[]) => {
+      if (acceptedFiles && acceptedFiles.length > 0) {
+        const file = acceptedFiles[0];
+        field.onChange(file);
+        setPreviewUrl(URL.createObjectURL(file));
+      }
+    },
+  });
+
   return (
-    <Controller
-      name={fieldPrefix}
-      control={control}
-      render={({ field }) => {
-        const onDrop = (acceptedFiles: File[]) => {
-          if (acceptedFiles && acceptedFiles.length > 0) {
-            const file = acceptedFiles[0];
-            field.onChange(file);
-            setPreviewUrl(URL.createObjectURL(file));
-          }
-        };
-
-        const { getRootProps, getInputProps, isDragActive } = useDropzone({
-          onDrop,
-          multiple: false,
-          accept: {
-            "image/jpeg": [".jpeg", ".jpg"],
-            "image/png": [".png"],
-          },
-        });
-
-        return (
-          <div className="flex w-full flex-col gap-[14px]">
-            <p className="text-[22px] font-medium">Attach Supporting Maps.</p>
-            <div
-              {...getRootProps()}
-              className={`cursor-pointer rounded-[30px] border border-dashed p-6 text-center transition ${
-                isDragActive ? "border-blue-500 bg-blue-50" : "border-gray-300"
-              }`}
-            >
-              <input {...getInputProps()} />
-              {field.value ? (
-                <>
-                  {previewUrl && (
-                    <div className="relative flex h-[300px] w-full items-center justify-center">
-                      <LazyImage
-                        src={previewUrl}
-                        alt="map"
-                        sizes="30px"
-                        fill
-                        className="object-contain"
-                      />
-                    </div>
-                  )}
-                  <p className="text-sm font-medium text-gray-700">
-                    {field.value.name}
-                  </p>
-                </>
-              ) : (
-                <div className="flex h-[256px] flex-col items-center justify-center gap-[20px]">
-                  <FaUpload className="size-[70px]" />
-                  <div className="flex flex-col gap-1">
-                    <p className="text-white">Drag and Drop your image</p>
-                    <p className="text-white">or</p>
-                    <p className="text-primary">Browse Files</p>
-                  </div>
-                </div>
-              )}
-            </div>
-            {!field.value && (
-              <p className="text-[12px] font-medium text-[#AAAAAA]">
-                Accepted file types: JPEG/JPG, PNG
-              </p>
+    <div className="flex w-full flex-col gap-[14px]">
+      <p className="text-[22px] font-medium">Attach Supporting Maps.</p>
+      <div
+        {...getRootProps()}
+        className={`cursor-pointer rounded-[30px] border border-dashed p-6 text-center transition ${
+          isDragActive ? "border-blue-500 bg-blue-50" : "border-gray-300"
+        }`}
+      >
+        <input {...getInputProps()} />
+        {field.value ? (
+          <>
+            {previewUrl && (
+              <div className="relative flex h-[300px] w-full items-center justify-center">
+                <LazyImage
+                  src={previewUrl}
+                  alt="map"
+                  sizes="30px"
+                  fill
+                  className="object-contain"
+                />
+              </div>
             )}
+            <p className="text-sm font-medium text-gray-700">
+              {field.value.name}
+            </p>
+          </>
+        ) : (
+          <div className="flex h-[256px] flex-col items-center justify-center gap-[20px]">
+            <FaUpload className="size-[70px]" />
+            <div className="flex flex-col gap-1">
+              <p className="text-white">Drag and Drop your image</p>
+              <p className="text-white">or</p>
+              <p className="text-primary">Browse Files</p>
+            </div>
           </div>
-        );
-      }}
-    />
+        )}
+      </div>
+      {!field.value && (
+        <p className="text-[12px] font-medium text-[#AAAAAA]">
+          Accepted file types: JPEG/JPG, PNG
+        </p>
+      )}
+    </div>
   );
 };
